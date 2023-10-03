bcb3a6d646e0a3f5d0623d0209262965408ac4fa
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-10-03 11:39:59 -0500

    Create class SeederDb and edit program

---
+++ Login/Login/DAL/SeederDb.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics.Metrics;
+using Login.DAL.Entities;
+using Login.Enum;
+using Login.Helpers;
+
+
+namespace Login.DAL
+{
+    public class SeederDb
+    {
+        private readonly DataBaseContext _context;
+        private readonly IUserHelper _userHelper;
+        
+
+        public SeederDb(DataBaseContext context, IUserHelper userHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+            
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await PopulateRolesAsync();
+            await PopulateUserAsync("Sebastian", "Londoño", "sebas@yopmail.com", "3142393101", "Barbosa", "1035234145", UserType.Admin);
+            await PopulateUserAsync("Jessica", "Gomez", "jess@yopmail.com", "3142393101", "Barbosa", "1035232261", UserType.User);
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task PopulateRolesAsync()
+        {
+            await _userHelper.AddRoleAsync(UserType.Admin.ToString());
+            await _userHelper.AddRoleAsync(UserType.User.ToString());
+        }
+
+        private async Task PopulateUserAsync(string firstName, string lastName, string email, string phone, string address, string document, UserType userType)
+        {
+            User user = await _userHelper.GetUserAsync(email);
+            if (user == null)
+            {
+              
+
+                user = new User
+                {
+                    CreatedDate = DateTime.Now,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    UserName = email,
+                    PhoneNumber = phone,
+                    Address = address,
+                    Document = document,               
+                    UserType = userType
+               
+                };
+
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+            }
+        }
+
+
+   
+
+
+        
+
+
+
+
+
+
+    }
+}
--- Login/Login/Program.cs
+++ Login/Login/Program.cs
@@ -1,5 +1,11 @@
 using Login.DAL;
+using Login.DAL.Entities;
+using Login.Helpers;
+using Login.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +16,60 @@ builder.Services.AddDbContext<DataBaseContext>(
     o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+//Builder para llamar la clase SeederDb.cs|
+builder.Services.AddTransient<SeederDb>();
+
+//Builder para llamar la interfaz IUserHelper.cs
+builder.Services.AddScoped<IUserHelper, UserHelper>();
+
+
+var supportedCultures = new[]
+{
+    new CultureInfo("es-CO")
+};
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("es-CO");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
+
+builder.Services.AddIdentity<User, IdentityRole>(io =>
+{
+    io.User.RequireUniqueEmail = true;
+    io.Password.RequireDigit = false;
+    io.Password.RequiredUniqueChars = 0;
+    io.Password.RequireLowercase = false;
+    io.Password.RequireNonAlphanumeric = false;
+    io.Password.RequireUppercase = false;
+    io.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<DataBaseContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Unauthorized";
+    options.AccessDeniedPath = "/Account/Unauthorized";
+});
+
+
 var app = builder.Build();
+
+app.UseRequestLocalization();
+
+SeederData();
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDb? service = scope.ServiceProvider.GetService<SeederDb>();
+        service.SeedAsync().Wait();
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
